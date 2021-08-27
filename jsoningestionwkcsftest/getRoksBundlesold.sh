0520972d7f9795b86f7ddd0d320c6e0928b849b1
Russell Anderson <rga@us.ibm.com>
2021-08-27 16:29:46 -0400

    new stuff

---
+++ jsoningestionwkcsftest/getRoksBundlesold.sh
@@ -0,0 +1,4 @@
+curl -v -L -X GET https://cp4d-cpd-cp4d.wkc-statefarm-73aebe06726e634c608c4167edcc2aeb-0000.us-south.containers.appdomain.cloud/ibm/iis/igc-rest/v1/bundles/ \
+-H 'cache-control: no-cache' \
+-H 'Accept: application/json' \
+-u rga:Tuesda0y?
